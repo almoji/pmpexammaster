7d70a33d00417f90a0e0fdfa21084db6f7e73dbc
Alberto Moya <alberto@local>
2026-07-21 19:48:13 +0400

    Add JSON serialization to QuestionAttempt

--- lib/models/question_attempt.dart
+++ lib/models/question_attempt.dart
@@ -1,16 +1,10 @@
 class QuestionAttempt {
   final int questionId;
-
   final DateTime timestamp;
-
   final bool correct;
-
   final List<String> selectedAnswers;
-
   final int elapsedSeconds;
-
   final bool practiceMode;
-
   final bool mockMode;
 
   QuestionAttempt({
@@ -22,4 +16,28 @@ class QuestionAttempt {
     required this.practiceMode,
     required this.mockMode,
   });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'questionId': questionId,
+      'timestamp': timestamp.toIso8601String(),
+      'correct': correct,
+      'selectedAnswers': selectedAnswers,
+      'elapsedSeconds': elapsedSeconds,
+      'practiceMode': practiceMode,
+      'mockMode': mockMode,
+    };
+  }
+
+  factory QuestionAttempt.fromJson(Map<String, dynamic> json) {
+    return QuestionAttempt(
+      questionId: json['questionId'],
+      timestamp: DateTime.parse(json['timestamp']),
+      correct: json['correct'],
+      selectedAnswers: List<String>.from(json['selectedAnswers']),
+      elapsedSeconds: json['elapsedSeconds'],
+      practiceMode: json['practiceMode'],
+      mockMode: json['mockMode'],
+    );
+  }
 }
