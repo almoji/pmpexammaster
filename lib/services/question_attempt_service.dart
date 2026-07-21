e9e712bc8e4770d460c5b0d26cb2f9c3605e6573
Alberto Moya <alberto@local>
2026-07-21 20:17:27 +0400

    Add QuestionAttempt persistence

--- lib/services/question_attempt_service.dart
+++ lib/services/question_attempt_service.dart
@@ -1,12 +1,29 @@
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
 import '../models/question_attempt.dart';
 
 class QuestionAttemptService {
+  static const String _storageKey = 'question_attempts';
+
   final List<QuestionAttempt> _attempts = [];
 
   List<QuestionAttempt> get attempts => List.unmodifiable(_attempts);
 
-  void addAttempt(QuestionAttempt attempt) {
+  Future<void> addAttempt(QuestionAttempt attempt) async {
     _attempts.add(attempt);
+    await saveAttempts();
+  }
+
+  Future<void> saveAttempts() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    final jsonList = _attempts
+        .map((attempt) => jsonEncode(attempt.toJson()))
+        .toList();
+
+    await prefs.setStringList(_storageKey, jsonList);
   }
 
   void clear() {
