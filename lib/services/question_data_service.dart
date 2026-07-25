727174a893313654aa3d8054f97a1b56438f44a2
Alberto Moya <alberto@local>
2026-07-26 01:07:49 +0400

    Add questions up to ID 3600 and fix question loading service

--- lib/services/question_data_service.dart
+++ lib/services/question_data_service.dart
@@ -7,20 +7,33 @@ class QuestionDataService {
   static const List<String> _questionFiles = [
 
     'assets/questions/questions_0001_0200.json',
-
     'assets/questions/questions_0201_0400.json',
-
     'assets/questions/questions_0401_0600.json',
-
     'assets/questions/questions_0601_0800.json',
-
     'assets/questions/questions_0801_1000.json',
-
     'assets/questions/questions_1001_1200.json',
-
     'assets/questions/questions_1201_1400.json',
-
     'assets/questions/questions_1401_1600.json',
+    'assets/questions/questions_1601_1800.json',
+    'assets/questions/questions_1801_2000.json',
+    'assets/questions/questions_2001_2200.json',
+    'assets/questions/questions_2201_2400.json',
+    'assets/questions/questions_2401_2600.json',
+    'assets/questions/questions_2601_2800.json',
+    'assets/questions/questions_2801_3000.json',
+    'assets/questions/questions_3001_3200.json',
+    'assets/questions/questions_3201_3400.json',
+    'assets/questions/questions_3401_3600.json',
+
+
+
+
+
+
+
+
+
+
 
     // Add new files here:
     // 'assets/questions/questions_0601_0800.json',
